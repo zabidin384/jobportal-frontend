494df4ec33b926e60087b099ed1277d935b73524
Zainal Abidin <zabidin384@gmail.com>
2026-01-03 00:47:48 +0700

    Change color status applied amd add cursor not allowed

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -1,12 +1,16 @@
 function StatusBadge({ status }) {
 	const statusConfig = {
-		Applied: "bg-gray-100 text-gray-800",
+		Applied: "bg-blue-100 text-blue-800",
 		Interview: "bg-yellow-100 text-yellow-800",
 		Hired: "bg-green-100 text-green-800",
 		Rejected: "bg-red-100 text-red-800",
 	};
 
-	return <span className={`px-3 py-1 rounded text-sm font-medium ${statusConfig[status] || "bg-gray-100 text-gray-800"}`}>{status}</span>;
+	return (
+		<span className={`px-3 py-1 rounded text-sm font-medium cursor-not-allowed ${statusConfig[status] || "bg-gray-100 text-gray-800"}`}>
+			{status}
+		</span>
+	);
 }
 
 export default StatusBadge;
